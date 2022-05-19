83c80851574299452291c7c883acec2686b736b1
dr-neptune <mrose4@worcester.edu>
2022-05-19 19:50:22 -0400

    last section

--- repl.py
+++ repl.py
@@ -102,10 +102,15 @@ data = api.get_candles('EUR/USD', period='m5',
                        start='2018-06-01 00:00:00',
                        stop='2018-06-30 00:00:00')
 
+data = api.get_candles('EUR/USD', period='m5',
+                       start='2018-06-01 00:00:00',
+                       stop='2018-06-30 00:00:00')
+
 data.iloc[-5:, 4:]
 
 # calculate the average bid-ask spread
 spread = (data['askclose'] - data['bidclose']).mean()
+
 
 # calculate the mid close prices from the ask and bid close prices
 data['midclose'] = (data['askclose'] + data['bidclose']) / 2
@@ -118,7 +123,9 @@ data['midclose'].plot(legend=True)
 plt.show()
 
 # create binarized data
-data['returns'] = np.log(data['midclose'] / data['midclose'].shift(1)).dropna()
+data['returns'] = np.log(data['midclose'] / data['midclose'].shift(1))
+
+data = data.dropna()
 
 lags = 5
 
@@ -145,7 +152,7 @@ from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 
 model = SVC(C=1, kernel='linear', gamma='auto')
-split = int(len(data) * .8)
+split = int(len(data) * .80)
 train = data.iloc[:split].copy()
 test = data.iloc[split:].copy()
 
@@ -174,3 +181,192 @@ test[['returns', 'strategy', 'strategy_tc']].sum().apply(np.exp)
 
 test[['returns', 'strategy', 'strategy_tc']].cumsum().apply(np.exp).plot()
 plt.show()
+
+## Optimal Leverage
+# annualized mean returns
+mean = test[['returns', 'strategy_tc']].mean() * len(data) * 12
+mean
+
+# annualized variances
+var = test[['returns', 'strategy_tc']].var() * len(data) * 12
+var
+
+# annualized volatilities
+vol = var ** 0.5
+vol
+
+# optimal leverage according to the Kelly criterion (full Kelly)
+mean / var
+
+# optional leverage (half Kelly)
+mean / var ** 0.5
+
+# performance of algorithmic trading strategy for different leverage values
+to_plot = ['returns', 'strategy_tc']
+
+for lev in list(range(10, 60, 10)):
+    label = f'lstrategy_tc_{lev}'
+    test[label] = test['strategy_tc'] * lev
+    to_plot.append(label)
+
+test[to_plot].cumsum().apply(np.exp).plot()
+plt.show()
+
+# Risk Analysis
+# initial equity
+equity = 3333
+
+# relevant log returns time series
+risk = pd.DataFrame(test['lstrategy_tc_30'])
+
+# scaled by the initial equity
+risk['equity'] = risk['lstrategy_tc_30'].cumsum().apply(np.exp) * equity
+
+# cumulative maximum values over time
+risk['cummax'] = risk['equity'].cummax()
+
+# drawdown values over time
+risk['drawdown'] = risk['cummax'] - risk['equity']
+
+# max drawdown value
+risk['drawdown'].max()
+
+# point in time when it happens
+t_max = risk['drawdown'].idxmax()
+t_max
+
+# identifies highs for which the drawdown must be 0
+temp = risk['drawdown'][risk['drawdown'] == 0]
+
+# calculates the timedelta values between all highs
+periods = (temp.index[1:].to_pydatetime() -
+           temp.index[:-1].to_pydatetime())
+
+# the longest drawdown period in seconds
+t_per = periods.max()
+
+
+risk[['equity', 'cummax']].plot()
+plt.axvline(t_max, c='r', alpha=0.5)
+plt.show()
+
+# VaR
+import scipy.stats as scs
+
+# define percentiles to check
+percs = np.array([0.01, 0.1, 1., 2.5, 5.0, 10.0])
+risk['returns'] = np.log(risk['equity'] / risk['equity'].shift(1))
+
+# calculate the VaR values given the percentile values
+VaR = scs.scoreatpercentile(equity * risk['returns'], percs)
+
+def print_var():
+    print('%16s %16s' % ('Confidence Level', 'Value-at-Risk'))
+    print(33 * '-')
+    for pair in zip(percs, VaR):
+        # translate percentile values into confidence levels and the VaR values to positive values
+        print('%16.2f %16.3f' % (100 - pair[0], -pair[1]))
+
+print_var()
+
+# calculate the VaR values for a time horizon of one hour
+
+# resample the data from 5 minute to 1 hour bars
+hourly = risk.resample('1H', label='right').last()
+hourly['returns'] = np.log(hourly['equity'] / hourly['equity'].shift(1))
+
+# recalculate the VaR values for the resampled data
+VaR = scs.scoreatpercentile(equity * hourly['returns'], percs)
+
+print_var()
+
+# persisting the model object
+import pickle
+
+pickle.dump(model, open('algorithm.pkl', 'wb'))
+
+# Online Algorithm
+algorithm = pickle.load(open('algorithm.pkl', 'rb'))
+
+sel = ['tradeId', 'amountK', 'currency', 'grossPL', 'isBuy']
+
+def print_positions(pos):
+    print('\n\n' + 50 * '=')
+    print('Going {}.\n'.format(pos))
+    time.sleep(1.5)  # wait for order to be executed and reflected in the open positions
+    print(api.get_open_positions()[sel])  # print open positions
+    print(50 * '=' + '\n\n')
+
+# set params
+symbol = 'EUR/USD'      # instrument symbol to be traded
+bar = '15s'             # bar length for resampling
+amount = 100            # the amount, in thousands, to be traded
+position = 0            # the initial position ('neutral')
+min_bars = lags + 1     # minimum # of resampled bars required for the first prediction and trade to be possible
+df = pd.DataFrame()     # empty DF to be used later for the resampled data
+
+def automated_strategy(data, dataframe):
+    global min_bars, position, df
+    # captures the length of the dataframe obj with the tick data
+    ldf = len(dataframe)
+    # resamples the tick data to the defined bar length
+    df = dataframe.resample(bar, label='right').last().ffill()
+    if ldf % 20 == 0:
+        print('%3d' % len(dataframe), end=',')
+    if len(df) > min_bars:
+        min_bars = len(df)
+        df['Mid'] = df[['Bid', 'Ask']].mean(axis=1)
+        df['Returns'] = np.log(df['Mid'] / df['Mid'].shift(1))
+        df['Direction'] = np.where(df['Returns'] > 0, 1, -1)
+        # picks the relevant feature values for all lags
+        features = df['Direction'].iloc[-(lags + 1):-1]
+        # reshape them to a form that the model can use for prediction
+        features = features.values.reshape(1, -1)
+        # generate prediction value (+-1)
+        signal = algorithm.predict(features)[0]
+
+        # conditions to enter (or keep) a long position
+        if position in [0, -1] and signal == 1:
+            api.create_market_buy_order(symbol, amount - position * amount)
+            position = 1
+            print_positions('LONG')
+        # conditions to enter (or keep) a short position
+        elif position in [0, -1] and signal == -1:
+            api.create_market_sell_order(symbol, amount + position * amount)
+            position = -1
+            print_positions('SHORT')
+
+    # condition to stop trading and close out any open positions
+    if len(dataframe) > 350:
+        api.unsubscribe_market_data('EUR/USD')
+        api.close_all()
+
+
+# copied from book, still not using "data" param
+# def automated_strategy(data, dataframe):
+#     global min_bars, position, df
+#     ldf = len(dataframe)
+#     df = dataframe.resample(bar, label='right').last().ffill()
+#     if ldf % 20 == 0:
+#         print('%3d' % len(dataframe), end=',')
+#     if len(df) > min_bars:
+#         min_bars = len(df)
+#         df['Mid'] = df[['Bid', 'Ask']].mean(axis=1)
+#         df['Returns'] = np.log(df['Mid'] / df['Mid'].shift(1))
+#         df['Direction'] = np.where(df['Returns'] > 0, 1, -1)
+#         features = df['Direction'].iloc[-(lags + 1):-1]
+#         features = features.values.reshape(1, -1)
+#         signal = algorithm.predict(features)[0]
+#         if position in [0, -1] and signal == 1:
+#             api.create_market_buy_order(
+#                 symbol, amount - position * amount)
+#             position = 1
+#             print_positions('LONG')
+#         elif position in [0, 1] and signal == -1:
+#             api.create_market_sell_order(
+#                 symbol, amount + position * amount)
+#             position = -1
+#             print_positions('SHORT')
+#     if len(dataframe) > 350:
+#         api.unsubscribe_market_data('EUR/USD')
+#         api.close_all()
