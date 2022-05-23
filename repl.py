6ccde7cb71ffad99fed6ce7db23f044d662f943a
dr-neptune <mrose4@worcester.edu>
2022-05-22 21:07:51 -0400

    it is very hot outside today

--- repl.py
+++ repl.py
@@ -388,3 +388,460 @@ plt.plot(srd.time_grid, srd.get_instrument_values()[:, :10])
 plt.axhline(me_srd.get_constant('theta'), color='r', ls='--', lw=2.0)
 plt.xticks(rotation=30)
 plt.show()
+
+# ch 19: derivatives valuation
+class ValuationClass:
+    '''basic class for single-factor valuation'''
+    def __init__(self, name, underlying, mar_env, payoff_func=''):
+        self.name = name
+        self.pricing_date = mar_env.pricing_date
+        try:
+            self.strike = mar_env.get_constant('strike')
+        except:
+            pass
+        self.maturity = mar_env.get_constant('maturity')
+        self.currency = mar_env.get_constant('currency')
+        self.frequency = underlying.frequency
+        self.paths = underlying.paths
+        self.discount_curve = underlying.discount_curve
+        self.payoff_func = payoff_func
+        self.underlying = underlying
+        self.underlying.special_dates.extend([self.pricing_date, self.maturity])
+
+    def update(self, initial_value=None, volatility=None, strike=None, maturity=None):
+        if initial_value is not None:
+            self.underlying.update(initial_value=initial_value)
+        if volatility is not None:
+            self.underlying.update(volatility=volatility)
+        if strike is not None:
+            self.strike = strike
+        if maturity is not None:
+            self.maturity = maturity
+            if maturity not in self.underlying.time_grid:
+                self.underlying.special_dates.append(maturity)
+                self.underlying.instrument_values = None
+
+    def delta(self, interval=None, accuracy=4):
+        if interval is None:
+            interval = self.underlying.initial_value / 50.
+
+        # forward difference approximation
+        # calculate left value for numerical delta
+        value_left = self.present_value(fixed_seed=True)
+        # numerical underlying value for right value
+        initial_del = self.underlying.initial_value + interval
+        self.underlying.update(initial_value=initial_del)
+        # calculate right value for numerical delta
+        value_right = self.present_value(fixed_seed=True)
+        # reset the initial value of the simulation object
+        self.underlying.update(initial_value=initial_del - interval)
+        delta = (value_right - value_left) / interval
+
+        if delta < -1.0:
+            return -1.0
+        elif delta > 1.0:
+            return 1.0
+        else:
+            return round(delta, accuracy)
+
+    def vega(self, interval=0.01, accuracy=4):
+        if interval < self.underlying.volatility / 50.:
+            interval = self.underlying.volatility / 50.
+
+        # forward difference approximation
+        value_left = self.present_value(fixed_seed=True)
+        vola_del = self.underlying.volatility + interval
+        self.underlying.update(volatility=vola_del)
+        value_right = self.present_value(fixed_seed=True)
+        self.underlying.update(volatility=vola_del - interval)
+        vega = (value_right - value_left) / interval
+        return round(vega, accuracy)
+
+# The Valuation Class
+class ValuationMCSEuropean(ValuationClass):
+    '''class to value European options with arbitrary payoff by single-factor Monte Carlo simulation'''
+    def generate_payoff(self, fixed_seed=False):
+        try:
+            strike = self.strike
+        except AttributeError:
+            pass
+        paths = self.underlying.get_instrument_values(fixed_seed=fixed_seed)
+        time_grid = self.underlying.time_grid
+        try:
+            time_index = np.where(time_grid == self.maturity)[0]
+            time_index = int(time_index)
+        except:
+            print('Maturity date not in time grid of underlying')
+        maturity_value = paths[time_index]
+        # average value over whole path
+        mean_value = np.mean(paths[:time_index], axis=1)
+        # maximum value over whole path
+        max_value = np.amax(paths[:time_index], axis=1)[-1]
+        # minimum value over whole path
+        min_value = np.amin(paths[:time_index], axis=1)[-1]
+        try:
+            payoff = eval(self.payoff_func)
+            return payoff
+        except:
+            print('Error evaluation payoff function')
+
+    def present_value(self, accuracy=6, fixed_seed=False, full=False):
+        cash_flow = self.generate_payoff(fixed_seed=fixed_seed)
+        discount_factor = self.discount_curve.get_discount_factors((self.pricing_date, self.maturity))[0, 1]
+        result = discount_factor * np.sum(cash_flow) / len(cash_flow)
+        if full:
+            return round(result, accuracy), discount_factor * cash_flow
+        else:
+            return round(result, accuracy)
+## Use Case
+me_gbm = MarketEnvironment('me_gbm', dt.datetime(2020, 1, 1))
+me_gbm.add_constant('initial_value', 36.)
+me_gbm.add_constant('volatility', 0.2)
+me_gbm.add_constant('final_date', dt.datetime(2020, 12, 31))
+me_gbm.add_constant('currency', 'EUR')
+me_gbm.add_constant('frequency', 'M')
+me_gbm.add_constant('paths', 10000)
+me_gbm.add_curve('discount_curve', ConstantShortRate('csr', 0.06))
+
+gbm = GeometricBrownianMotion('gbm', me_gbm)
+
+# define the market environment for the option itself
+me_call = MarketEnvironment('me_call', me_gbm.pricing_date)
+me_call.add_constant('strike', 40.)
+me_call.add_constant('maturity', dt.datetime(2020, 12, 31))
+me_call.add_constant('currency', 'EUR')
+
+payoff_func = 'np.maximum(maturity_value - strike, 0)'
+
+eur_call = ValuationMCSEuropean('eur_call', underlying=gbm, mar_env=me_call, payoff_func=payoff_func)
+
+eur_call.present_value()  # present value of European call option
+eur_call.delta()          # numerical estimate of the delta of the option. Delta is positive for calls
+eur_call.vega()           # numerical estimate of the vega for the option. Vega is positive for both calls and puts
+
+def plot_option_stats(s_list, p_list, d_list, v_list):
+    '''plot option prices, deltas, and vegas for a set of different initial values of the underlying'''
+    plt.figure(figsize=(10, 7))
+    sub1 = plt.subplot(311)
+    plt.plot(s_list, p_list, 'ro', label='present value')
+    plt.plot(s_list, p_list, 'b')
+    plt.legend(loc=0)
+    plt.setp(sub1.get_xticklabels(), visible=False)
+    sub2 = plt.subplot(312)
+    plt.plot(s_list, d_list, 'go', label='Delta')
+    plt.plot(s_list, d_list, 'b')
+    plt.legend(loc=0)
+    plt.ylim(min(d_list) - 0.1, max(d_list) + 0.1)
+    plt.setp(sub2.get_xticklabels(), visible=False)
+    sub3 = plt.subplot(313)
+    plt.plot(s_list, v_list, 'yo', label='Vega')
+    plt.plot(s_list, v_list, 'b')
+    plt.xlabel('initial value of underlying')
+    plt.legend(loc=0)
+    plt.show()
+
+s_list = np.arange(34., 46.1, 2.)
+p_list, d_list, v_list = [], [], []
+
+for s in s_list:
+    eur_call.update(initial_value=s)
+    p_list.append(eur_call.present_value(fixed_seed=True))
+    d_list.append(eur_call.delta())
+    v_list.append(eur_call.vega())
+
+# present value, delta, and vega estimates for European call option
+plot_option_stats(s_list, p_list, d_list, v_list)
+
+# consider a payoff that is a mixture of a regular and Asian payoff
+payoff_func = 'np.maximum(0.33 * (maturity_value + max_value) - 40, 0)'
+
+eur_as_call = ValuationMCSEuropean('eur_as_call', underlying=gbm, mar_env=me_call, payoff_func=payoff_func)
+
+s_list = np.arange(34., 46.1, 2.)
+p_list, d_list, v_list = [], [], []
+
+for s in s_list:
+    eur_as_call.update(s)
+    p_list.append(eur_as_call.present_value(fixed_seed=True))
+    d_list.append(eur_as_call.delta())
+    v_list.append(eur_as_call.vega())
+
+plot_option_stats(s_list, p_list, d_list, v_list)
+
+# American Exercise
+class ValuationMCSAmerican(ValuationClass):
+    '''class to value American options with arbitrary payoff by single-factor Monte Carlo simulation'''
+    def generate_payoff(self, fixed_seed=False):
+        try:
+            strike = self.strike
+        except AttributeError:
+            pass
+        paths = self.underlying.get_instrument_values(fixed_seed=fixed_seed)
+        time_grid = self.underlying.time_grid
+        time_index_start = int(np.where(time_grid == self.pricing_date)[0])
+        time_index_end = int(np.where(time_grid == self.maturity)[0])
+        instrument_values = paths[time_index_start:time_index_end + 1]
+        payoff = eval(self.payoff_func)
+        return instrument_values, payoff, time_index_start, time_index_end
+
+    def present_value(self, accuracy=6, fixed_seed=False, bf=5, full=False):
+        instrument_values, inner_values, time_index_start, time_index_end = self.generate_payoff(fixed_seed=fixed_seed)
+        time_list = self.underlying.time_grid[time_index_start:time_index_end + 1]
+        discount_factors = self.discount_curve.get_discount_factors(time_list, dtobjects=True)
+        V = inner_values[-1]
+        for t in range(len(time_list) - 2, 0, -1):
+            # derive relevant discount factor for given time interval
+            df = discount_factors[t, 1] / discount_factors[t + 1, 1]
+            # regression step
+            rg = np.polyfit(instrument_values[t], V * df, bf)
+            # calculation of continuation values per path
+            C = np.polyval(rg, instrument_values[t])
+            # optimal decision step
+            # if condition (inner value > regression cont value)
+            # then take inner value; ow take actual value
+            V = np.where(inner_values[t] > C, inner_values[t], V * df)
+        df = discount_factors[0, 1] / discount_factors[1, 1]
+        result = df * np.sum(V) / len(V)
+        if full:
+            return round(result, accuracy), df * V
+        else:
+            return round(result, accuracy)
+
+## a use case
+me_gbm = MarketEnvironment('me_gbm', dt.datetime(2020, 1, 1))
+me_gbm.add_constant('initial_value', 36.)
+me_gbm.add_constant('volatility', 0.2)
+me_gbm.add_constant('final_date', dt.datetime(2020, 12, 31))
+me_gbm.add_constant('currency', 'EUR')
+me_gbm.add_constant('frequency', 'W')
+me_gbm.add_constant('paths', 50000)
+me_gbm.add_curve('discount_curve', ConstantShortRate('csr', 0.06))
+
+gbm = GeometricBrownianMotion('gbm', me_gbm)
+payoff_func = 'np.maximum(strike - instrument_values, 0)'
+
+me_am_put = MarketEnvironment('me_am_put', dt.datetime(2020, 1, 1))
+me_am_put.add_constant('maturity', dt.datetime(2020, 12, 31))
+me_am_put.add_constant('strike', 40.)
+me_am_put.add_constant('currency', 'EUR')
+
+am_put = ValuationMCSAmerican('am_put', underlying=gbm, mar_env=me_am_put, payoff_func=payoff_func)
+
+am_put.present_value(fixed_seed=True, bf=5)
+
+# chapter 20: portfolio valuation
+
+# derivatives positions
+class DerivativesPosition:
+    '''
+    class to model a derivatives position
+
+    name: name of object
+    quantity: number of assets/derivatives making up the position
+    underlying: name of asset/risk factor for the derivative
+    mar_env: instance of market environment
+    otype: valuation class to use
+    payoff_func: payoff string for the derivative
+    '''
+    def __init__(self, name, quantity, underlying, mar_env, otype, payoff_func):
+        self.name = name
+        self.quantity = quantity
+        self.underlying = underlying
+        self.mar_env = mar_env
+        self.otype = otype
+        self.payoff_func = payoff_func
+
+    def get_info(self):
+        print('NAME')
+        print(self.name, '\n')
+        print('QUANTITY')
+        print(self.quantity, '\n')
+        print('UNDERLYING')
+        print(self.underlying, '\n')
+        print('MARKET ENVIRONMENT')
+        print('\n**Constants**')
+        for key, value in self.mar_env.constants.items():
+            print(key, value)
+        print('\n**Lists**')
+        for key, value in self.mar_env.lists.items():
+            print(key, value)
+        print('\n**Curves**')
+        for key, value in self.mar_env.curves.items():
+            print(key, value)
+        print('\nOPTION TYPE')
+        print(self.otype, '\n')
+        print('PAYOFF FUNCTION')
+        print(self.payoff_func)
+
+# a use case
+me_gbm = MarketEnvironment('me_gbm', dt.datetime(2020, 1, 1))
+me_gbm.add_constant('initial_value', 36.)
+me_gbm.add_constant('volatility', 0.2)
+me_gbm.add_constant('currency', 'EUR')
+me_gbm.add_constant('model', 'gbm')
+
+me_am_put = MarketEnvironment('me_am_put', dt.datetime(2020, 1, 1))
+me_am_put.add_constant('maturity', dt.datetime(2020, 12, 31))
+me_am_put.add_constant('strike', 40.)
+me_am_put.add_constant('currency', 'EUR')
+
+payoff_func = 'np.maximum(strike - instrument_values, 0)'
+
+am_put_pos = DerivativesPosition(name='am_put_pos',
+                                 quantity=3,
+                                 underlying='gbm',
+                                 mar_env=me_am_put,
+                                 otype='American',
+                                 payoff_func=payoff_func)
+
+am_put_pos.get_info()
+
+# Derivatives Portfolios
+models = {'gbm': GeometricBrownianMotion,
+          'jd': JumpDiffusion,
+          'srd': SquareRootDiffusion}
+
+otypes = {'European': ValuationMCSEuropean,
+          'American': ValuationMCSAmerican}
+
+class DerivativesPortfolio:
+    '''
+    class for modeling and valuing portfolios of derivatives positions
+
+    name: name of the object
+    positions: dictionary of positions
+    val_env: market environment for the valuation
+    assets: dictionary of market environments for the assets
+    correlations: correlations between assets
+    fixed_seed: flag for fixed random number generator seed
+    '''
+    def __init__(self, name, positions, val_env, assets, correlations=None, fixed_seed=False):
+        self.name = name
+        self.positions = positions
+        self.val_env = val_env
+        self.assets = assets
+        self.underlyings = set()
+        self.correlations = correlations
+        self.time_grid = None
+        self.underlying_objects = {}
+        self.valuation_objects = {}
+        self.fixed_seed = fixed_seed
+        self.special_dates = []
+        for pos in self.positions:
+            # determine the earliest starting date
+            self.val_env.constants['starting_date'] = min(self.val_env.constants['final_date'],
+                                                          positions[pos].mar_env.pricing_date)
+            # determine the latest date of relevance
+            self.val_env.constants['final_date'] = max(self.val_env.constants['final_date'],
+                                                       positions[pos].mar_env['maturity'])
+            # collect all underlyings and add to set (for uniqueness)
+            self.underlyings.add(positions[pos].underlying)
+
+        # generate general time grid
+        start = self.val_env.constants['starting_ate']
+        end = self.val_env.constants['final_date']
+        time_grid = list(pd.date_range(start=start,
+                                       end=end,
+                                       freq=self.val_env.constants['frequency']).to_pydatetime())
+
+        for pos in self.positions:
+            maturity_date = positions[pos].mar_env.constants['maturity']
+            if maturity_date not in time_grid:
+                time_grid.insert(0, maturity_date)
+                self.special_dates.append(maturity_date)
+
+        if start not in time_grid:
+            time_grid.inset(0, start)
+        if end not in time_grid:
+            time_grid.append(end)
+
+        # delete duplicates
+        time_grid = list(set(time_grid))
+        time_grid.sort()
+
+        self.time_grid = np.array(time_grid)
+        self.val_env.add_list('time_grid', self.time_grid)
+
+        if correlations is not None:
+            ul_list = sorted(self.underlyings)
+            correlation_matrix = np.zeros((len(ul_list), len(ul_list)))
+            np.fill_diagonal(correlation_matrix, 1.0)
+            correlation_matrix = pd.DataFrame(correlation_matrix, index=ul_list, columns=ul_list)
+
+            for i, j in correlations:
+                corr = min(corr, 0.999999)
+                # fill correlation matrix
+                correlation_matrix.loc[i, j] = corr
+                correlation_matrix.loc[j, i] = corr
+
+            # determine Cholesky Matrix
+            cholesky_matrix = np.linalg.cholesky(np.array(correlation_matrix))
+
+            # dict with index positions for the slice of the random number array to be used by respective underlying
+            rn_set = {asset: ul_list.index(asset) for asset in self.underlyings}
+
+            # random numbers array, to be used by all underlyings (if correlations exist)
+            random_numbers = sn_random_numbers((len(rn_set),
+                                                len(self.time_grid),
+                                                self.val_env.constants['paths']),
+                                                fixed_seed=fixed_seed)
+
+            # add all to valuation environment that is to be shared with every underlying
+            self.val_env.add_list('cholesky_matrix', cholesky_matrix)
+            self.val_env.add_list('random_numbers', random_numbers)
+            self.val_env.add_list('rn_set', rn_set)
+
+        for asset in self.underlyings:
+            # select market environment of asset
+            mar_env = self.assets[asset]
+            # add valuation environment to market environment
+            mar_env.add_environment(val_env)
+            # select right simulation class
+            model = models[mar_env.constants['model']]
+            # instantiate simulation class
+            if correlations is not None:
+                self.underlying_objects[asset] = model(asset, mar_env, corr=True)
+            else:
+                self.underlying_objects[asset] = model(asset, mar_env, corr=False)
+
+        for pos in positions:
+            # select right valuation class (European, American)
+            val_class = otypes[positions(pos).otype]
+            # pick market environment and add valuation environment
+            mar_env = positions[pos].mar_env
+            mar_env.add_environment(self.val_env)
+            # instantiate valuation class
+            self.valuation_objects[pos] = val_class(name=positions[pos].name,
+                                                    mar_env=mar_env,
+                                                    underlying=self.underlying_objects[positions[pos].underlying],
+                                                    payoff_func=payoff_func)
+
+    def get_positions(self):
+        '''convenience method to get information about all derivative positions in a portfolio'''
+        for pos in self.positions:
+            bar = '\n' + 50 * '-'
+            print(bar)
+            self.positions[pos].get_info()
+            print(bar)
+
+    def get_statistics(self, fixed_seed=False):
+        '''provides portfolio statistics'''
+        res_list = []
+        # iterate over all positions in a portfolio
+        for pos, value in self.valuation_objects.items():
+            p = self.positions[pos]
+            pv = value.present_value(fixed_seed=fixed_seed)
+            res_list.append([p.name,
+                             p.quantity,
+                             pv,
+                             value.currency,
+                             pv * p.quantity,
+                             value.delta() * p.quantity,
+                             value.vega() * p.quantity])
+
+        # generate a dataframe with all results
+        res_df = pd.DataFrame(res_list, columns=['name', 'quant', 'value', 'curr', 'pos_value', 'pos_delta', 'pos_vega'])
+        return res_df
+
+
+# a use case
